e10d11178c13814465334a1bff24c941b47986d3
Aleksander Szymaszek <aleksander.szymaszek@gmail.com>
2016-03-17 22:03:25 +0100

    Change: added test names to print test result statement

--- src/test_cases.py
+++ src/test_cases.py
@@ -20,27 +20,27 @@ class DemoQaTests():
         elements = self.driver.find_elements_by_xpath("//div[@id='secondary']/aside")
         try:
             assert len(elements) == 4
-            print("Test pass number of elements found is {}".format(len(elements)))
+            print("Test widget_area pass number of elements found is {}".format(len(elements)))
         except AssertionError:
-            print("Assertion failed number of elements found is {}".format(elements))
+            print("Assertion failed in widget_area number of elements found is {}".format(elements))
 
     def test_widget_list(self):
 
         elements = self.driver.find_elements_by_xpath("//ul[@id='menu-widget']/li")
         try:
             assert len(elements) == 7
-            print("Test pass number of elements found is {}".format(len(elements)))
+            print("Test widget_list pass number of elements found is {}".format(len(elements)))
         except AssertionError:
-            print("Assertion failed number of elements found is {}".format(elements))
+            print("Assertion failed in widget list number of elements found is {}".format(elements))
 
     def test_interaction(self):
 
         elements = self.driver.find_elements_by_xpath("//ul[@id='menu-interactions']/li")
         try:
             assert len(elements) == 5
-            print("Test pass number of elements found is {}".format(len(elements)))
+            print("Test interaction pass number of elements found is {}".format(len(elements)))
         except AssertionError:
-            print("Assertion failed number of elements found is {}".format(elements))
+            print("Assertion failed in interaction number of elements found is {}".format(elements))
 
     def test_contact_form(self):
 
@@ -49,9 +49,9 @@ class DemoQaTests():
         elements = self.driver.find_elements_by_xpath("//form[@class='wpcf7-form']/p")
         try:
             assert len(elements) == 5
-            print("Test pass number of elements found is {}".format(len(elements)))
+            print("Test contact_form pass number of elements found is {}".format(len(elements)))
         except AssertionError:
-            print("Assertion failed number of elements found is {}".format(elements))
+            print("Assertion failed in contact_form number of elements found is {}".format(elements))
 
     def main(self):
         self.setUp()
